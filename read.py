15536eb24544bc7198bf9d23dfda92a18c437b20
Alex Patel <alexanderpatel@college.harvard.edu>
2014-10-25 01:47:07 -0400

    read.py - get contents for all raw html files

--- read.py
+++ read.py
@@ -18,17 +18,20 @@
 
 import os
 import csv
+import re
 
 from bs4 import BeautifulSoup
 
-raw_html_dir = "/raw_html"
+raw_html_dir = "/raw_html/"
 wd = os.getcwd()
 
 def get_filenames():
     """ Return filenames of raw html files in raw_html_dir. """
 
     raw_html_path = os.getcwd() + raw_html_dir
-    files = os.listdir(path)
+    files = os.listdir(raw_html_path)
+    fnum = lambda x : int(os.path.splitext(x)[0])
+    files = sorted(files, key=lambda x : fnum(x)) # sort by file number 
     return files
 
 def read_files(files):
@@ -36,16 +39,21 @@ def read_files(files):
         Reads raw html from files and returns list of (filename, 
         BeautifulSoup(file)) tuples. 
     """
-
     fcontents = []
+    nfiles = len(files)
+    nread = 0
+    print '>> READING RAW HTML FILES'
     for fname in files:
         fp = wd + raw_html_dir + fname
         f = open(fp, 'r')
         try:
             raw_html = f.read()
             fcontents.append((fname, BeautifulSoup(raw_html)))
+            print '\t' + fname 
+            nread += 1
         finally:
             f.close()
+    print "{0} of {1} files read".format(nread, nfiles)
     return fcontents
 
 def write(data, fname):
@@ -53,3 +61,5 @@ def write(data, fname):
         writer = csv.writer(f, delimiter=',')
         for line in data:
             writer.writerow(line)
+
+read_files(get_filenames())
