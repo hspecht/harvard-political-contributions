9b8e4a86d65f25d35c950e3d1d8e147b76b4ed13
Alex Patel <alexanderpatel@college.harvard.edu>
2014-11-20 14:42:37 -0500

    update 13-14 data to reflect contributions through Nov. 20

--- build.R
+++ build.R
@@ -3,7 +3,7 @@
 #
 
 # directory names
-raw_data.path    <- "data/raw_data"
+raw_data.path    <- "raw_data"
 header.path  <- "data/header"
 # data filenames
 indiv.fname  <- "itcont.txt"
@@ -16,6 +16,7 @@ set.headers <- function() {
     indiv.header <- "indiv_header_file.csv"
     cm.header    <- "cm_header_file.csv"
     cn.header    <- "cn_header_file.csv"
+    # set global header file frames
     indiv_header <<- read.csv("indiv_header_file.csv", header = FALSE)
     cm_header <<- read.csv("cm_header_file.csv", header = FALSE)
     cn_header <<- read.csv("cn_header_file.csv", header = FALSE)
@@ -27,33 +28,32 @@ read.data <- function(year_dir) {
     ## Assumes that there exists in year_dir: itcont.txt, cm.txt, cn.txt
     print(year_dir)
     # move to data/raw_data/year_dir
-    setwd("data")
+    #setwd("data")
     setwd(paste(raw_data.path, year_dir, sep="/"))
     # read individual contribution file
-    print("... read individual contributions")
+    print("Read individual contributions")
     indiv_contribs <- read.table(file = "itcont.txt", header = FALSE, sep = "|", quote = "\"", comment.char = "")
     names(indiv_contribs) <- as.character(unlist(indiv_header[1,]))
     # reduce to 'Harvard' employees
     indiv_contribs <- indiv_contribs[grep("HARVARD", indiv_contribs$EMPLOYER),]
     # read committee master file
-    print("... read committee master")
+    print("Read committee master")
     cm <- read.table(file = "cm.txt", header = FALSE, sep = "|", quote = "\"", comment.char = "")
     names(cm) <- as.character(unlist(cm_header[1,]))
     # read candidate master file
-    print("... read candidate master")
+    print("Read candidate master")
     cn <- read.table(file = "cn.txt", header = FALSE, sep = "|", quote = "", comment.char = "")
     names(cn) <- as.character(unlist(cn_header[1,]))
     # merge data frames
-    print("... merging")
+    print("Merging")
     iccm <- merge(x = indiv_contribs, y = cm, by = "CMTE_ID", all.x = TRUE)
     iccmcn <- merge(x = iccm, y = cn, by = "CAND_ID", all.x = TRUE)
     # return to project dir
-    setwd("../../../")
+    setwd("../../")
     return(iccmcn)
 }
 
 read.all <- function() {
-    setwd("data")
     set.headers()
     # run on all raw_data folders "/XX-YY" and output "XX-YY.csv"
     for (i in seq(14, 2, -2)) {
